31cc8eec4ef296f53e83822d6e025868a8b62487
Christian Trabold <christian.trabold@dkd.de>
2012-06-27 15:50:16 +0200

    Add PPA recipe to register official repo
    This recipe avoids hardcoded repo integration in the recipe.

---
+++ attributes/ppa.rb
@@ -0,0 +1,2 @@
+default['redis']['ppa_uri'] = 'http://ppa.launchpad.net/chris-lea/redis-server/ubuntu'
+default['redis']['ppa_key'] = 'C7917B12'
---
+++ recipes/ppa.rb
@@ -0,0 +1,27 @@
+#
+# Author:: Christian Trabold <christian.trabold@dkd.de>
+# Cookbook Name:: redis
+# Attributes:: ppa
+#
+# Copyright 2012, dkd Internet Service GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+apt_repository 'redis-server' do
+  uri node['redis']['ppa_uri']
+  distribution node['lsb']['codename']
+  components ['main']
+  keyserver 'keyserver.ubuntu.com'
+  key node['redis']['ppa_key']
+  action :add
+end
